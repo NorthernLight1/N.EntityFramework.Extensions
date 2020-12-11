56847daae8785d05385501884534dffee23debe2
Timo Tschupp <timo.tschupp@kronos.com>
2020-12-11 09:31:15 -0500

    Added missing files: Contstants.cs

---
+++ N.EntityFramework.Extensions/Common/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N.EntityFramework.Extensions.Common
+{
+    public static class Constants
+    {
+        public readonly static string Guid_ColumnName = "_be_xx_id";
+    }
+}
