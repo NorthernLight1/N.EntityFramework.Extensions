55605027fbdc0914affe07ce02db8231414db1a9
Timo Tschupp <timo.tschupp@kronos.com>
2020-12-14 18:06:48 -0500

    Added AutoMapOutputIdentity option to BulkInsert() function [Author:nqbjnh(https://github.com/nqbjnh)]

--- N.EntityFramework.Extensions/Data/BulkInsertOptions.cs
+++ N.EntityFramework.Extensions/Data/BulkInsertOptions.cs
@@ -8,11 +8,17 @@ namespace N.EntityFramework.Extensions
     public class BulkInsertOptions<T> : BulkOptions
     {
         public Expression<Func<T, object>> InputColumns { get; set; }
+        public bool AutoMapOutputIdentity { get; set; }
         public bool KeepIdentity { get; set; }
 
         public string[] GetInputColumns()
         {
             return this.InputColumns == null ? null : this.InputColumns.Body.Type.GetProperties().Select(o => o.Name).ToArray();
+        }
+
+        public BulkInsertOptions()
+        {
+            this.AutoMapOutputIdentity = true;
         }
     }
 }
