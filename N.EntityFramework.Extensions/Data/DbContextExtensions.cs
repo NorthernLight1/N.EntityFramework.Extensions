4c874dc5517d02918d0e3a025f9c5db43ad41be3
Timo Tschupp <timo.tschupp@kronos.com>
2021-03-02 23:37:50 -0500

    Added new test case BulkMerge Renamed test case from BulkMerge_Options_Default to BulkMerge_Options_OnMergeCondition Moved some utility functions from Data/DbContextExtensions.cs to Util/CommonUtil.cs Added a new overload method for BulkMerge so that options are optional

--- N.EntityFramework.Extensions/Data/DbContextExtensions.cs
+++ N.EntityFramework.Extensions/Data/DbContextExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using N.EntityFramework.Extensions.Common;
 using N.EntityFramework.Extensions.Sql;
+using N.EntityFramework.Extensions.Util;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -195,6 +196,11 @@ namespace N.EntityFramework.Extensions
             };
         }
 
+        public static BulkMergeResult<T> BulkMerge<T>(this DbContext context, IEnumerable<T> entities)
+        {
+            return BulkMerge(context, entities, new BulkMergeOptions<T>());
+        }
+
         public static BulkMergeResult<T> BulkMerge<T>(this DbContext context, IEnumerable<T> entities, BulkMergeOptions<T> options)
         {
             int rowsAffected = 0;
@@ -235,7 +241,7 @@ namespace N.EntityFramework.Extensions
                     }
 
                     string mergeSqlText = string.Format("MERGE {0} t USING {1} s ON ({2}) WHEN NOT MATCHED BY TARGET THEN INSERT ({3}) VALUES ({3}) WHEN MATCHED THEN UPDATE SET {4} OUTPUT {5};",
-                        destinationTableName, stagingTableName, options.MergeOnCondition.ToSqlPredicate("s", "t"),
+                        destinationTableName, stagingTableName, options.GetMergeOnConditionSql(storeGeneratedColumnNames),
                         SqlUtil.ConvertToColumnString(columnsToInsert),
                         SqlUtil.ConvertToColumnString(columnstoUpdate),
                         SqlUtil.ConvertToColumnString(columnsToOutput)
@@ -558,62 +564,6 @@ namespace N.EntityFramework.Extensions
         {
             return context.Database.Connection as SqlConnection;
         }
-
-        private static string ToSqlPredicate<T>(this Expression<T> expression, params string[] parameters)
-        {
-            var stringBuilder = new StringBuilder((string)expression.Body.GetPrivateFieldValue("DebugView"));
-            int i = 0;
-            foreach (var expressionParam in expression.Parameters)
-            {
-                if (parameters.Length <= i) break;
-                stringBuilder.Replace((string)expressionParam.GetPrivateFieldValue("DebugView"), parameters[i]);
-                i++;
-            }
-            stringBuilder.Replace("&&", "AND");
-            stringBuilder.Replace("==", "=");
-            return stringBuilder.ToString();
-        }
-        private static string ToSqlUpdateSetExpression<T>(this Expression<T> expression, string tableName)
-        {
-            List<string> setValues = new List<string>();
-            var memberInitExpression = expression.Body as MemberInitExpression;
-            foreach (var binding in memberInitExpression.Bindings)
-            {
-                string constantValue = GetExpressionValueAsString<T>(binding);
-
-                setValues.Add(string.Format("[{0}].[{1}]={2}", tableName, binding.Member.Name, constantValue));
-            }
-            return string.Join(",", setValues);
-        }
-
-        private static string GetExpressionValueAsString<T>(MemberBinding binding)
-        {
-            var value = GetExpressionValue(binding);
-
-            if (value == null)
-                return "NULL";
-            if (value is string str)
-                return "'" + str.Replace("'", "''") + "'";
-            if (value is bool b)
-                return b ? "1" : "0";
-            if (value is DateTime dt)
-                return "'" + dt.ToString("yyyy-MM-ddTHH:mm:ss.fff") + "'"; // Convert to ISO-8601
-            if (!value.GetType().IsClass)
-                return Convert.ToString(value, CultureInfo.InvariantCulture);
-
-            throw new NotImplementedException("Unhandled data type.");
-        }
-
-        private static object GetExpressionValue(MemberBinding binding)
-        {
-            if (binding.GetPrivateFieldValue("Expression") is ConstantExpression constantExpression)
-            {
-                return constantExpression.Value;
-            }
-            
-            return Expression.Lambda(binding.GetPrivateFieldValue("Expression") as Expression).Compile().DynamicInvoke();
-        }
-
         public static TableMapping GetTableMapping(this IObjectContextAdapter context, Type type)
         {
             var metadata = context.ObjectContext.MetadataWorkspace;
