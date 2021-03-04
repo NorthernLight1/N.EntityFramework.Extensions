35f1541dbb79561518a09459e857a3da387b56e7
Timo Tschupp <timo.tschupp@kronos.com>
2021-03-04 00:22:34 -0500

    Added new test case BulkInsert_Options_InsertIfNotExists Added new logic to test case BulkInsert_Options_KeepIdentity to test for duplicate data error Added InsertOnCondition and InsertIfNotExists fields to BulkMergeOption.cs Added new funcationality to BulkInsert so that it will insert data only if it does not exists (BulkMergeOption.InsertIfNotExists=true) Added logic to Util/CommonUtil.cs for constructing the join condition sql. BulkInsert and BulkMerge both this this function now. Added Key attribute to Test/Data/Order.cs so that it doesn't allow duplicate data Removed GetMergeOnConditionSql() from BulkMergeOption.cs since it is now a utility function

--- N.EntityFramework.Extensions.Test/Data/Order.cs
+++ N.EntityFramework.Extensions.Test/Data/Order.cs
@@ -1,7 +1,10 @@
-﻿namespace N.EntityFramework.Extensions.Test.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace N.EntityFramework.Extensions.Test.Data
 {
     public class Order
     {
+        [Key]
         public long Id { get; set; }
         public string ExternalId { get; set; }
         public decimal Price { get; set; }
---
+++ N.EntityFramework.Extensions/Util/CommonUtil.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N.EntityFramework.Extensions.Util
+{
+    internal static class CommonUtil<T>
+    {
+        internal static string GetJoinConditionSql(Expression<Func<T, T, bool>> joinKeyExpression, string[] storeGeneratedColumnNames, string sourceTableName="s", string targetTableName="t")
+        {
+            string joinConditionSql = string.Empty;
+            if (joinKeyExpression != null)
+            {
+                joinConditionSql = joinKeyExpression.ToSqlPredicate(sourceTableName, targetTableName);
+            }
+            else
+            {
+                int i = 1;
+                foreach (var storeGeneratedColumnName in storeGeneratedColumnNames)
+                {
+                    joinConditionSql += (i > 1 ? "AND" : "") + string.Format("{0}.{2}={1}.{2}", sourceTableName, targetTableName, storeGeneratedColumnName);
+                    i++;
+                }
+            }
+            return joinConditionSql;
+        }
+    }
+}
