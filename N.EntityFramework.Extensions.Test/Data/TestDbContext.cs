dce4240a3c40e6a9cb177ce3d783a7c4614ec6e5
Timo Tschupp <timo.tschupp@kronos.com>
2021-03-10 15:55:57 -0500

    Added connection string in Tests/Data/TestDbContext.cs to override EF 6.2.0 default connection string Reduced the dependency from EF 6.4.4+ to 6.2.0+ Added a fix for getting the database connection from DBContext or IQuerable since it was crashing for EF 6.2.0

--- N.EntityFramework.Extensions.Test/Data/TestDbContext.cs
+++ N.EntityFramework.Extensions.Test/Data/TestDbContext.cs
@@ -6,10 +6,11 @@ namespace N.EntityFramework.Extensions.Test.Data
 {
     public class TestDbContext : DbContext
     {
+        private static readonly string _connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog = N.EntityFramework.Extensions.Test.Data.TestDbContext; Integrated Security = True; MultipleActiveResultSets=True";
         public virtual DbSet<Order> Orders { get; set;  }
         public virtual DbSet<Article> Articles { get; set;  }
 
-        public TestDbContext()
+        public TestDbContext() : base(_connectionString)
         {
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<TestDbContext, TestDbConfiguration>());
         }
