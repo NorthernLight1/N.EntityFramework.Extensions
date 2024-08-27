d8d41390b3e7b38845675f26e5ce10523811eb3c
NorthernLight1 <49600465+NorthernLight1@users.noreply.github.com>
2024-08-27 11:32:48 -0400

    Fixed Issue With Database Migration Error

--- N.EntityFramework.Extensions.Test/Data/TestDbContext.cs
+++ N.EntityFramework.Extensions.Test/Data/TestDbContext.cs
@@ -4,7 +4,7 @@ namespace N.EntityFramework.Extensions.Test.Data;
 
 public class TestDbContext : DbContext
 {
-    private static readonly string _connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog = N.EntityFramework.Extensions.Test.Data.TestDbContext; Integrated Security = True; MultipleActiveResultSets=True";
+    private static readonly string _connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog = N.EntityFramework.Extensions.TestDbContext; Integrated Security = True; MultipleActiveResultSets=True";
     public virtual DbSet<Order> Orders { get; set; }
     public virtual DbSet<Product> Products { get; set; }
     public virtual DbSet<ProductCategory> ProductCategories { get; set; }
